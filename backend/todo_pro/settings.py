1922d2600b864f1886f3f7aa369fb2710cb374f4
dyavathrocky <davathrak@gmail.com>
2020-07-11 19:48:49 +0530

    backend

--- backend/todo_pro/settings.py
+++ backend/todo_pro/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#3rd party 
+    'rest_framework',
+    'corsheaders',
+
+#local 
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Todo
 
 # Create your views here.
+
+from .serializers import TodoSerializer
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
